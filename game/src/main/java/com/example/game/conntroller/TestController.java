42febe64ba2b627692d791c62dd70c2fb2360832
liufujun <android-sw19@top-tech.com.cn>
2021-07-27 15:19:38 +0800

    去除多余注解

--- game/src/main/java/com/example/game/conntroller/TestController.java
+++ game/src/main/java/com/example/game/conntroller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.context.annotation.SessionScope;
+
 
 
 import java.util.ArrayList;
@@ -42,42 +42,19 @@ public class TestController {
         return "templates";
     }
 
-//    @RequestMapping("/paly2")
-//    public String paly2(Model model,@RequestParam("msg") String msg){
-//        int 校验码=(int)(Math.random()*100000);
-//        校验码List.add(校验码);
-//        model.addAttribute("list",select());
-//        if (!msg.equals("0")) {
-//            model.addAttribute("MSG", msg);
-//        }else {
-//            model.addAttribute("MSG", "卡点10:00:00游戏");
-//        }
-//        model.addAttribute("jym",校验码);
-//        return "templates";
-//    }
 
     @RequestMapping("/insertsql")
     public String insert(@RequestParam("name1") String name,
                        @RequestParam("achievement1") String achievement,
                        @RequestParam("content1") String content,
-//                         @RequestParam("jym") int 校验码,
                        ModelMap model,
-                         Model model2,
                          SessionStatus sessionStatus,
                        @RequestParam("city1") String city){
-
-//        if (校验码List.indexOf(校验码)==-1&&校验码!=0){
-//            return "redirect:/paly2?msg=6666666666";
-//        }else {
-//            校验码List.remove(校验码List.indexOf(校验码));
-//        }
         String jym;
         try {
             jym=model.get("jym").toString();
         }catch (Exception e){
             sessionStatus.setComplete();
-//            return "redirect:/paly2?msg=6666666666";
-//            model2.addAttribute("MSG", "你真厉害，还开挂");
             System.out.println("没有这个校验码");
             return "404";
         }
@@ -99,7 +76,6 @@ public class TestController {
         //调用他的update方法-
         System.out.println(sql);
         jdbcTemplate.execute(sql);
-        model2.addAttribute("MSG", "卡点10:00:00游戏");
         return "redirect:/paly";
     }
 
